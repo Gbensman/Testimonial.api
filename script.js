9f071289bc5f694048b8392f1db1f49510ae626b
Gbensman <115927260+Gbensman@users.noreply.github.com>
2022-10-23 12:44:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,54 @@
-let contain = document.querySelector('.contain')
+let contain =document.querySelector('.contain')
+let prev = document.querySelector('.btn-prev')
+let next = document.querySelector('.btn-next')
 let apiData = 0
-const displayTestimonial=(data)=>{
-    contain.innerHTML=`<div class="card p-3">
-    <div class="image">
-        <img src="${data[0].avatar}" alt="">
-    </div>
-    <div class="card-text">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus molestiae accusamus non voluptate maxime perspiciatis, eligendi minus sint officiis ipsam.</p>
-        <div class="play">
-            <div class="btn btn-outline-success">play audio</div>
-        </div>
-    </div>
-    <div class="card-btn d-flex justify-content-between"  >
-        <button class="btn">prev</button>
-        <button class="btn">next</button>
-    </div>
-</div>`
-}
 
-let endpoint = 'https://testimonialapi.toolcarton.com/api'
+
+
+let endpoint ='https://testimonialapi.toolcarton.com/api'
 fetch(endpoint)
 .then((response)=>{
     return response.json();
 })
 .then((data)=>{
-    console.log(data);
-    displayTestimonial(data)
+    const displayTestimonial=()=>{
+        let result = ` <div class="card p-3">
+        <div class="image d-flex justify-content-between">
+            <div><img src="${data[apiData].avatar}" alt=""></div>
+            <div class="profile">
+            <h3>${data[apiData].name}</h3>
+            <h4>${data[apiData].designation}</h4>
+            <h5>${data[apiData].location}</h5>
+            <h6>Rating: ${data[apiData].rating}</h6>
+        </div>
+        </div>
+        <div class="card-text">
+            <p>${data[apiData].message}</p>
+            <div class="play">
+                <button class="btn btn-outline-success">Play Audio</button>
+            </div>
+        </div>
+    </div>`
+    
+    return result;
+    }
+    
+    
+    let display = displayTestimonial();
+    contain.innerHTML = display
+    
+
+
+
+    next.addEventListener("click", (e) => {
+        e.preventDefault();
+        // next.disabled = false;
+        if (apiData >= 0 && data[apiData].id <= data.length - 1) {
+          apiData++;
+          displayTestimonial();
+          let display = displayTestimonial();
+          contain.innerHTML = display
+          console.log(data[apiData].id);
+        }
+      });
 })
